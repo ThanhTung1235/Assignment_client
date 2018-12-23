e29a284c1022a38b6601fc17cbd666f4c2f6dc48
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-12-24 00:49:42 +0700

    handle save file

---
+++ StudentApp/Entity/TokenResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp.Entity
+{
+    class TokenResponse
+    {
+        public string _token { get; set; }
+    }
+}
--- StudentApp/Views/StudentPage.xaml.cs
+++ StudentApp/Views/StudentPage.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using StudentApp.Entity;
+using StudentApp.Service;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,6 +32,36 @@ namespace StudentApp.Views
             this.InitializeComponent();
         }
 
+        public async void GetInfoUser()
+        {
+            //lay token tu file ra
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await storageFolder.GetFileAsync("token.txt");
+            var content = await FileIO.ReadTextAsync(file);
+            TokenResponse tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(content);
+
+            HttpClient httpClient = new HttpClient();
+            var response = httpClient.GetAsync(APIHandle.GET_INFO_USER);
+            var responseContext = await response.Result.Content.ReadAsStringAsync();
+            Student student = JsonConvert.DeserializeObject<Student>(responseContext);
+            this.txt_email.Text = student.Email;
+            this.txt_phone.Text = student.Phone;
+            int gender_user = student.Gender;
+            switch (gender_user)
+            {
+                case 2:
+                    this.txt_gender.Text = "Nữ";
+                    break;
+                case 1:
+                    this.txt_gender.Text = "Nam";
+                    break;
+                case 0:
+                    this.txt_gender.Text = "Giới tính khác";
+                    break;
+            }
+
+
+        }
         private void btn_change_info(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Views.ChangeInfo));
